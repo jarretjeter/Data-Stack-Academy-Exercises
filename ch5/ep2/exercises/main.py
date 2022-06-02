d37a883aeeb5ac28c70bdf8a949faedaab663440
Jarret Jeter <jarretjeter@yahoo.com>
2022-06-02 16:16:52 -0700

    read yaml file and print colored lines, and build docker image

--- ch5/ep2/exercises/main.py
+++ ch5/ep2/exercises/main.py
@@ -1,3 +1,6 @@
+from colored import fg, bg, attr
+import yaml
+
 # Exercise #1: potato salad image
 
 # Create a docker image that prints the recipe for making potato salad.
@@ -8,11 +11,24 @@
 # 1. Create a default `CMD` for your image that runs your script by default
 # 1. Additional points: use the pypi [tabulate](https://pypi.org/project/tabulate/) package to print the nutritional facts in a table format
 
-import yaml
 
-def load_config(path_to_yaml):
+
+def load_yaml(path_to_yaml):
     """
-    Function to read args out of a yaml file
+    Print the lines of a yaml file
     """
-    with open(path_to_yaml) as open_yaml:
-        return yaml.full_load(open_yaml)
+    with open(path_to_yaml) as file:
+        recipes = yaml.full_load(file)
+        for key, value in recipes.items():
+            color = bg("blue") + fg("yellow")
+            color2 = fg("purple_4a")
+            reset = attr("reset")
+            # print(f"%s {key} %s" % (fg(1), attr(0)), ":", f"%s%s {value} %s" % (fg(1), bg(15), attr(0)))
+            # print(f"%s {key} %s" % (fg(1), attr(0)), ":", value)
+            print(f"{color} + {key} + {reset}", ":", f"{color2} + {value} + {reset}")
+
+def run():
+    load_yaml("data/potato_salad.yml")
+
+if __name__=="__main__":
+    run()
